5ba4c997bb31ce7efdd0e8d3895e3e40626e649e
Vitali Sazanovich <vitali.sazanovich@koop.overheid.nl>
2025-04-26 20:25:53 +0200

    Finish refactoring, use RayLib

--- go.mod
+++ go.mod
@@ -2,13 +2,7 @@ module tsp-hardware-test
 
 go 1.24
 
-require (
-	github.com/deckarep/golang-set/v2 v2.7.0
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20250409052854-a4292f0f0412
-	github.com/magiconair/properties v1.8.9
-	github.com/tevino/abool/v2 v2.1.0
-	github.com/veandco/go-sdl2 v0.4.40
-)
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20250409052854-a4292f0f0412
 
 require (
 	github.com/ebitengine/purego v0.7.1 // indirect
--- src/main.go
+++ src/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"embed"
-	mapset "github.com/deckarep/golang-set/v2"
 	rl "github.com/gen2brain/raylib-go/raylib"
+	"math"
 )
 
 var (
@@ -11,43 +11,197 @@ var (
 	mediaList embed.FS
 )
 
+const (
+	upCode     = 1
+	rightCode  = 2
+	downCode   = 3
+	leftCode   = 4
+	xCode      = 5
+	aCode      = 6
+	bCode      = 7
+	yCode      = 8
+	l1Code     = 9
+	l2Code     = 10
+	r1Code     = 11
+	r2Code     = 12
+	selectCode = 13
+	menuCode   = 14
+	startCode  = 15
+)
+
+const (
+	SCREEN_LEFT_UP_X     = int32(361)
+	SCREEN_LEFT_UP_Y     = int32(192)
+	SCREEN_RIGHT_DOWN_X  = int32(919)
+	SCREEN_RIGHT_DOWN_Y  = int32(508)
+	SCREEN_WIDTH         = SCREEN_RIGHT_DOWN_X - SCREEN_LEFT_UP_X
+	SCREEN_HEIGHT        = SCREEN_RIGHT_DOWN_Y - SCREEN_LEFT_UP_Y
+	JOYSTICK_INITIAL_MAX = 0.9
+)
+
 func main() {
 	rl.InitWindow(1280, 720, "TrimUI Hardware Test")
-
-	file, _ := mediaList.Open("media/bgr.png")
-	stat, _ := file.Stat()
-	size := stat.Size()
-	buf := make([]byte, size)
-	if _, err := file.Read(buf); err != nil {
-		println(err.Error())
-	}
-
-	img := rl.LoadImageFromMemory(".png", buf, int32(size))
-	texTspPad := rl.LoadTextureFromImage(img)
-	pressedButtonCodes := mapset.NewSet[int32]()
-	shouldExit := false
 	rl.SetTargetFPS(60)
 
+	var (
+		gamePadId                                          int32   = 0
+		shouldExit                                                 = false
+		crossBytes                                                 = orPanicRes(mediaList.ReadFile("media/cross_yellow.png"))
+		crossTexture                                               = rl.LoadTextureFromImage(rl.LoadImageFromMemory(".png", crossBytes, int32(len(crossBytes))))
+		bgrBytes                                                   = orPanicRes(mediaList.ReadFile("media/bgr.png"))
+		bgrTexture                                                 = rl.LoadTextureFromImage(rl.LoadImageFromMemory(".png", bgrBytes, int32(len(bgrBytes))))
+		keyColor                                                   = rl.Lime
+		x1, y1, x2, y2                                     float64 = 0, 0, 0, 0
+		roundedX1, roundedY1, roundedX2, roundedY2         float64 = 0, 0, 0, 0
+		correctedX1, correctedY1, correctedX2, correctedY2 float64 = 0, 0, 0, 0
+		maxX1, maxY1, maxX2, maxY2                                 = JOYSTICK_INITIAL_MAX, JOYSTICK_INITIAL_MAX, JOYSTICK_INITIAL_MAX, JOYSTICK_INITIAL_MAX
+		minX1, minY1, minX2, minY2                                 = -JOYSTICK_INITIAL_MAX, -JOYSTICK_INITIAL_MAX, -JOYSTICK_INITIAL_MAX, -JOYSTICK_INITIAL_MAX
+	)
+
 	for !rl.WindowShouldClose() && !shouldExit {
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
+		rl.DrawTexture(bgrTexture, 0, 0, rl.White)
 
-		if rl.IsGamepadAvailable(0) {
+		//functional
+		if rl.IsGamepadButtonDown(gamePadId, menuCode) || rl.IsKeyDown(rl.KeyM) {
+			rl.DrawCircle(331, 484, 11, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, selectCode) || rl.IsKeyDown(rl.KeyS) {
+			rl.DrawCircle(947, 484, 11, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, startCode) || rl.IsKeyDown(rl.KeyT) {
+			rl.DrawCircle(991, 484, 11, keyColor)
+		}
+		//letters
+		if rl.IsGamepadButtonDown(gamePadId, aCode) || rl.IsKeyDown(rl.KeyA) {
+			rl.DrawCircle(1023, 292, 13, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, bCode) || rl.IsKeyDown(rl.KeyB) {
+			rl.DrawCircle(988, 327, 13, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, xCode) || rl.IsKeyDown(rl.KeyX) {
+			rl.DrawCircle(988, 258, 13, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, yCode) || rl.IsKeyDown(rl.KeyY) {
+			rl.DrawCircle(954, 292, 13, keyColor)
+		}
+		//arrows
+		if rl.IsGamepadButtonDown(gamePadId, leftCode) || rl.IsKeyDown(rl.KeyLeft) {
+			rl.DrawRectangle(249, 280, 25, 24, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, rightCode) || rl.IsKeyDown(rl.KeyRight) {
+			rl.DrawRectangle(303, 280, 25, 24, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, upCode) || rl.IsKeyDown(rl.KeyUp) {
+			rl.DrawRectangle(277, 253, 24, 24, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, downCode) || rl.IsKeyDown(rl.KeyDown) {
+			rl.DrawRectangle(277, 308, 24, 24, keyColor)
+		}
+		//shoulders
+		if rl.IsGamepadButtonDown(gamePadId, l1Code) || rl.IsKeyDown(rl.KeyOne) {
+			rl.DrawRectangleRounded(rl.Rectangle{X: 218, Y: 82, Width: 110, Height: 20}, 0.9, 10, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, r1Code) || rl.IsKeyDown(rl.KeyTwo) {
+			rl.DrawRectangleRounded(rl.Rectangle{X: 955, Y: 82, Width: 110, Height: 20}, 0.9, 10, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, l2Code) || rl.IsKeyDown(rl.KeyThree) {
+			rl.DrawRectangleRounded(rl.Rectangle{X: 235, Y: 45, Width: 90, Height: 20}, 0.9, 10, keyColor)
+		}
+		if rl.IsGamepadButtonDown(gamePadId, r2Code) || rl.IsKeyDown(rl.KeyFour) {
+			rl.DrawRectangleRounded(rl.Rectangle{X: 955, Y: 45, Width: 90, Height: 20}, 0.9, 10, keyColor)
+		}
+		//joysticks
+		x1 = float64(rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisLeftX))
+		y1 = float64(rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisLeftY))
+		x2 = float64(rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisRightX))
+		y2 = float64(rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisRightY))
 
-			pressedButtonCodes.Add(rl.GetGamepadButtonPressed())
-			for val := range pressedButtonCodes.Iter() {
-				if !rl.IsGamepadButtonDown(0, val) {
-					pressedButtonCodes.Remove(val)
-				}
-			}
-			if pressedButtonCodes.Contains(14) && pressedButtonCodes.Contains(15) {
-				shouldExit = true
-			}
+		maxX1 = math.Max(x1, maxX1)
+		maxY1 = math.Max(x1, maxY1)
+		maxX2 = math.Max(x2, maxX2)
+		maxY2 = math.Max(y2, maxY2)
+
+		minX1 = math.Min(x1, minX1)
+		minY1 = math.Min(x1, minY1)
+		minX2 = math.Min(x2, minX2)
+		minY2 = math.Min(y2, minY2)
+
+		roundedX1 = toFixed(x1, 3)
+		roundedY1 = toFixed(y1, 3)
+		roundedX2 = toFixed(x2, 3)
+		roundedY2 = toFixed(y2, 3)
+
+		correctedX1 = (x1 * 100 / If(x1 > 0, maxX1, -minX1)) / 100
+		correctedY1 = (y1 * 100 / If(y1 > 0, maxY1, -minY1)) / 100
+		correctedX2 = (x2 * 100 / If(x2 > 0, maxX2, -minX2)) / 100
+		correctedY2 = (y2 * 100 / If(y2 > 0, maxY2, -minY2)) / 100
+
+		if (roundedX1 != 0 && roundedY1 != 0) || rl.IsKeyDown(rl.KeyL) {
+			rl.DrawCircle(304, 412, 20, keyColor)
 		}
 
-		rl.DrawTexture(texTspPad, 0, 0, rl.White)
+		if (roundedX2 != 0 && roundedY2 != 0) || rl.IsKeyDown(rl.KeyR) {
+			rl.DrawCircle(977, 412, 20, keyColor)
+		}
+
+		if roundedX1 != 0 && roundedY1 != 0 {
+			rl.DrawTexture(
+				crossTexture,
+				SCREEN_LEFT_UP_X+(SCREEN_WIDTH-crossTexture.Width)/2+int32(float64((SCREEN_WIDTH-crossTexture.Width)/2)*correctedX1),
+				SCREEN_LEFT_UP_Y+(SCREEN_HEIGHT-crossTexture.Height)/2+int32(float64((SCREEN_HEIGHT-crossTexture.Height)/2)*correctedY1),
+				rl.White)
+		}
+		if roundedX2 != 0 && roundedY2 != 0 {
+			rl.DrawTexture(
+				crossTexture,
+				SCREEN_LEFT_UP_X+(SCREEN_WIDTH-crossTexture.Width)/2+int32(float64((SCREEN_WIDTH-crossTexture.Width)/2)*correctedX2),
+				SCREEN_LEFT_UP_Y+(SCREEN_HEIGHT-crossTexture.Height)/2+int32(float64((SCREEN_HEIGHT-crossTexture.Height)/2)*correctedY2),
+				rl.White)
+		}
+
+		//exit
+		if rl.IsGamepadButtonDown(gamePadId, menuCode) && rl.IsGamepadButtonDown(gamePadId, startCode) {
+			shouldExit = true //see WindowShouldClose, it checks if KeyEscape pressed or Close icon pressed
+		}
 		rl.EndDrawing()
 	}
-	rl.UnloadTexture(texTspPad)
+	rl.UnloadTexture(bgrTexture)
 	rl.CloseWindow()
+}
+
+func orPanic(err interface{}) {
+	switch v := err.(type) {
+	case error:
+		if v != nil {
+			panic(err)
+		}
+	case bool:
+		if !v {
+			panic("condition failed: != true")
+		}
+	}
+}
+
+func orPanicRes[T any](res T, err interface{}) T {
+	orPanic(err)
+	return res
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func If[T any](cond bool, vTrue, vFalse T) T {
+	if cond {
+		return vTrue
+	}
+	return vFalse
 }
